e5701be4f5a78579ec7427206489fae7d98c26a4
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-12-03 16:37:34 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,68 @@
+// import React, { Component, useState } from "react";
+// import { Switch, Route, Link, useLocation, Redirect } from "react-router-dom";
+// import "../styles/App.css";
+// import Home from "./Home";
+// import About from "./About";
+// import NoMatch from "./NoMatch";
+// import LocationDisplay from "./LocationDisplay";
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div id="main">
+//         <Link to="/about">About</Link> <br />
+//         <Link to="/">Home</Link>
+//         <LocationDisplay data-testid="location-display" />
+//         <Switch>
+//           <Route path="/about" component={About} />
+//           <Route exact path="/" component={Home} />
+//           <Route component={NoMatch} />
+//         </Switch>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
 import React, { Component, useState } from "react";
-import { Switch, Route, Link, useLocation, Redirect } from "react-router-dom";
+import { Route, Switch, useLocation, Link } from "react-router-dom";
 import "../styles/App.css";
-import Home from "./Home";
-import About from "./About";
-import NoMatch from "./NoMatch";
-import LocationDisplay from "./LocationDisplay";
+function LocationDisplay() {
+  let location = useLocation();
+  return <div data-testid="location-display">{location.pathname}</div>;
+}
+function Home() {
+  return (
+    <>
+      <div>You are home</div>
+    </>
+  );
+}
+function About() {
+  return (
+    <>
+      <div>You are on the about page</div>
+    </>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div id="main">
-        <Link to="/about">About</Link> <br />
-        {/* <a href="/about">About</a>  */}
-        {/* <a href="/">Home</a> */}
+        <Link to="/about">About</Link>
         <Link to="/">Home</Link>
-        <LocationDisplay data-testid="location-display" />
         <Switch>
-          <Route path="/about" component={About} />
+          <Route exact path="/about" component={About} />
           <Route exact path="/" component={Home} />
-          <Route component={NoMatch} />
+          <Route path="/" component={() => <div>No match</div>} />
         </Switch>
+        <LocationDisplay />
       </div>
     );
   }
 }
 
 export default App;
+// export const LocationDisplay = LocationDisplayComponent;
