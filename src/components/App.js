88f136f83547d13313a79e670e18ba47198d570f
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-12-03 12:31:08 +0530

    fin

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { Switch, Route, Link, useLocation } from "react-router";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import LocationDisplay from "./LocationDisplay";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        {/* <Link to="/about">About</Link>*/}
+        <a href="/about">About</a> <br />
+        <a href="/">Home</a>
+        <LocationDisplay />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    );
+  }
 }
-
 
 export default App;
