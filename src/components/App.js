66b74d0427013af629b0e90d3d4a7587e7f8a177
Parichay Pai <39957257+ParichayPai@users.noreply.github.com>
2020-12-03 12:48:45 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,24 @@
 import React, { Component, useState } from "react";
-import { Switch, Route, Link, useLocation } from "react-router";
+import { Switch, Route, Link, useLocation, Redirect } from "react-router-dom";
 import "../styles/App.css";
 import Home from "./Home";
 import About from "./About";
+import NoMatch from "./NoMatch";
 import LocationDisplay from "./LocationDisplay";
 
 class App extends Component {
   render() {
     return (
       <div id="main">
-        {/* <Link to="/about">About</Link>*/}
-        <a href="/about">About</a> <br />
-        <a href="/">Home</a>
+        <Link to="/about">About</Link> <br />
+        {/* <a href="/about">About</a>  */}
+        {/* <a href="/">Home</a> */}
+        <Link to="/">Home</Link>
         <LocationDisplay />
         <Switch>
           <Route path="/about" component={About} />
-          <Route path="/" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route component={NoMatch} />
         </Switch>
       </div>
     );
